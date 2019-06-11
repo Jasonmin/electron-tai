8c406cf6e5eb152262a0ff1960ed929fc935ad4c
Jasonmin <313410158@qq.com>
2019-06-11 16:41:42 +0800

    electron caibian formal

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
+  // mainWindow.loadFile('assets/sections/index.html')
   mainWindow.loadFile('index.html')
 
   // Open the DevTools.
