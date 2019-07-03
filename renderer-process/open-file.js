d5a21fd7d4850367b0f7561db21a44de67943784
Jasonmin <313410158@qq.com>
2019-07-03 18:01:27 +0800

    open file

---
+++ renderer-process/open-file.js
@@ -0,0 +1,12 @@
+const {ipcRenderer} = require('electron')
+
+const selectDirBtn = document.getElementById('select-directory')
+
+selectDirBtn.addEventListener('click', (event) => {
+    console.log(`select-directory`)
+  ipcRenderer.send('open-file-dialog')
+})
+
+ipcRenderer.on('selected-directory', (event, path) => {
+  document.getElementById('selected-file').innerHTML = `You selected: ${path}`
+})
