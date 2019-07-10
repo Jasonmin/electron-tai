dacda400d5ec7ede067ea3ecfb2739f54d8b67fb
Jasonmin <313410158@qq.com>
2019-07-10 10:02:32 +0800

    fix issue

--- renderer-process/create-window.js
+++ renderer-process/create-window.js
@@ -3,10 +3,10 @@ const path = require('path')
 
 const newWindowBtn = document.getElementById('new-window-person-detail')
 
-newWindowBtn.addEventListener('click', (event) => {
-    const modalPath = path.join('file://', __dirname, '../assets/modules/contact/memberDetail.html')
-    let win = new BrowserWindow({ width: 600, height: 600 })
-    win.on('close', () => { win = null })
-    win.loadURL(modalPath)
-    win.show()
-})
+// newWindowBtn.addEventListener('click', (event) => {
+//     const modalPath = path.join('file://', __dirname, '../assets/modules/contact/memberDetail.html')
+//     let win = new BrowserWindow({ width: 600, height: 600 })
+//     win.on('close', () => { win = null })
+//     win.loadURL(modalPath)
+//     win.show()
+// })
