9194586003cbe098dc172edb59f48dc1540d43d8
Jasonmin <313410158@qq.com>
2019-07-10 09:47:59 +0800

    little

--- renderer-process/create-window.js
+++ renderer-process/create-window.js
@@ -1,15 +1,12 @@
 const { BrowserWindow } = require('electron').remote
 const path = require('path')
 
-const newWindowBtn = document.getElementById('new-window')
+const newWindowBtn = document.getElementById('new-window-person-detail')
 
-// newWindowBtn.addEventListener('click', (event) => {
-//   console.log('llllllll')
-//   // const modalPath = path.join('file://', __dirname, '../assets/modules/contact/modal.html')
-//   const modalPath = path.join('file://', __dirname, '../assets/modules/contact/memberDetail.html')
-//   let win = new BrowserWindow({ width: 600, height: 600 })
-
-//   win.on('close', () => { win = null })
-//   win.loadURL(modalPath)
-//   win.show()
-// })
+newWindowBtn.addEventListener('click', (event) => {
+    const modalPath = path.join('file://', __dirname, '../assets/modules/contact/memberDetail.html')
+    let win = new BrowserWindow({ width: 600, height: 600 })
+    win.on('close', () => { win = null })
+    win.loadURL(modalPath)
+    win.show()
+})
