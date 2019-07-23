a4fbc4865aea40a74f1e18688c1c002dd9b8635a
Jasonmin <313410158@qq.com>
2019-07-23 15:06:26 +0800

    fix issue

---
+++ imbuild/app.config.js
@@ -0,0 +1,9 @@
+// window.__sealtalk_config={
+//   serverUrl:"http://localhost:8585",
+//   appkey:"appkey"
+// }
+
+window.__sealtalk_config = {
+    serverUrl: "http://api.sealtalk.im",
+    appkey: "n19jmcy59f1q9"
+}
--- main.js
+++ main.js
@@ -35,7 +35,8 @@ function createWindow() {
     // Create the browser window.
     var config = store.get('windowBounds')
     config.webPreferences = {
-        nodeIntegration: true,
+        // nodeIntegration: true,
+        nodeIntegration: false, // elec-zq 
         webSecurity: false
     }
 
@@ -79,7 +80,8 @@ function createWindow() {
 
 function loadTargetPage(loginData) {
     if (loginData) { // 已登录
-        mainWindow.loadFile('./imdist/index.html')
+        // mainWindow.loadFile('./imdist/index.html')
+        mainWindow.loadFile('./imbuild/index.html')
         global.loginModel = loginData
         global.loginUser = (JSON.parse(loginData)).data;
     } else { // 未登录
